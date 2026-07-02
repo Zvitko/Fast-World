8eaa6a21d422fd00187d6b997f6e8beeb6a8c7e6
Zvitko <zwvitko1@gmail.com>
2026-07-02 19:35:09 +0000

    Updating endpoints for metric reading

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
 
+Instrumentator().instrument(app).expose(app)
+
 @app.get("/")
 def root():
-    return {"application": "Fast World"}
-
-@app.get("/fast")
-def fast():
     return {
         "message": "This message comes from inside your container using FastAPI"
     }
 
 @app.get("/health")
 def health():
-    return {"status": "healthy"}
+    return {
+        "status": "healthy"
+    }
+
+@app.get("/ready")
+def ready():
+    return {
+        "status": "ready"
+    }
